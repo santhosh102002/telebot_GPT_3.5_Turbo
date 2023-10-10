7007ed3fa9f341e7b392276dcc7a33828516f549
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-10-10 10:25:01 +0530

    connecting to telegram using aiogram

---
+++ research/tele_echo_bot.py
@@ -0,0 +1,32 @@
+import logging
+from aiogram import Bot, Dispatcher,types,executor
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_TOKEN = os.getenv("TOKEN")
+
+#configuration
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start','help'])
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives messages with `/start` or `/help` command
+    """
+    await message.reply("Hi\nIam Echo Bot!\nPowered by aiogram")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    """
+    This will return echo
+    """
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
